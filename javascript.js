88e73e4328971f7b7f51a7e1df7623608ae714bb
malesgz <malenagonzalez959@gmail.com>
2023-05-29 11:55:42 -0600

    Ejercicios de arreglos

--- javascript.js
+++ javascript.js
@@ -1,7 +1,4 @@
-// FUNCIONES: 
-
-
- 1° EJERCICIO.
+// FUNCIONES: 1° EJERCICIO.
 function esMayorEdad(edad) {
     if (edad >= 18) {
       return true;
@@ -17,23 +14,65 @@ function calcularAreaRectangulo(base, altura) {
     let area = base * altura;
     return area
 }
-console.log(calcularAreaRectangulo(5, 10)); // Devuelve 50
-console.log(calcularAreaRectangulo(3, 7)); // Devuelve 21
+console.log(calcularAreaRectangulo(5, 10)); 
+console.log(calcularAreaRectangulo(3, 7)); 
 
 // 3° EJERCICIO.
 function esPalindromo(cadena) {
     let cadenaInvertida = cadena.split("").reverse().join("");
     return cadena === cadenaInvertida;
 }
-console.log(esPalindromo("radar")); // Devuelve true
-console.log(esPalindromo("hola")); // Devuelve false
+console.log(esPalindromo("radar")); 
+console.log(esPalindromo("hola")); 
 
 // 4° EJERCICIO.
 function generaNumeroAleatorio() {
-    var numeroAleatorio = Math.floor(Math.random() * 10) + 1;
-    return numeroAleatorio;
+  let numeroAleatorio = Math.floor(Math.random() * 10) + 1;
+  return numeroAleatorio;
 }
-console.log(generaNumeroAleatorio()); // Devuelve un número aleatorio entre 1 y 10
-console.log(generaNumeroAleatorio()); // Devuelve otro número aleatorio entre 1 y 10
+console.log(generaNumeroAleatorio()); 
+console.log(generaNumeroAleatorio()); 
+
+// EJERCICIOS DE ARREGLOS.
+// 1° EJERCICIO.
+function obtenerSuma(arreglo) {
+    let suma = 0;
+    for (let i = 0; i < arreglo.length; i++) {
+      suma += arreglo[i];
+    }
+    return suma;
+}
+console.log(obtenerSuma([1, 2, 3, 4, 5]));
+console.log(obtenerSuma([10, -5, 8, 3])); 
+
+// 2° EJERCICIO.
+function obtenerPares(arreglo) {
+    var pares = [];
+    for (let i = 0; i < arreglo.length; i++) {
+      if (arreglo[i] % 2 === 0) {
+        pares.push(arreglo[i]);
+      }
+    }
+    return pares;
+}
+console.log(obtenerPares([1, 2, 3, 4, 5, 6])); // Devuelve [2, 4, 6]
+console.log(obtenerPares([10, 15, 20, 25])); // // Devuelve [10, 20]
+
+// 3° EJERCICIO.
+function obtenerPromedioPonderado(notas, pesos) {
+    if (notas.length !== pesos.length) {
+        // El método throw reanuda la ejecución de un generador al lanzar un error en éste y regresar un objeto con las dos propiedades done y value.
+      throw new Error("Los arreglos deben tener la misma longitud.");
+    }
+    let totalNotas = 0;
+    let totalPesos = 0;
+    for (let i = 0; i < notas.length; i++) {
+      totalNotas += notas[i] * pesos[i];
+      totalPesos += pesos[i];
+    }
+    const promedioPonderado = totalNotas / totalPesos;
+    return promedioPonderado;
+}
+// 4° EJERCICIO.
 
   
